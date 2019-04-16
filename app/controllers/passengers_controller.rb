633ee6a3482d6e34fc326a97d01d51b48e02059f
Hana <hanalways@gmail.com>
2019-04-16 14:48:10 -0700

    work out bugs in form

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -14,8 +14,32 @@ class PassengersController < ApplicationController
 
   def create
     @passenger = Passenger.new(passenger_params)
-    @passenger.save
-    redirect_to passengers_path
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.update(passenger_params)
+      redirect_to passenger_path(@passenger.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+    @passenger.destroy
+    redirect_back fallback_location: :passengers_path
   end
 
   private
