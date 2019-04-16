86b7509d0a0c04a6a740c6ee688c4e57e990a7c3
Hana <hanalways@gmail.com>
2019-04-15 17:11:09 -0700

    Update view to list all drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,4 +1,8 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
   def show
     driver_id = params[:id]
     @driver = Driver.find_by(id: driver_id)
