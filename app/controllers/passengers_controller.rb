17bdb48da9fcb6b55ddc19f078cc9ab216749490
Hana <hanalways@gmail.com>
2019-04-16 12:23:21 -0700

    update scaffolding code for show

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,7 +4,7 @@ class PassengersController < ApplicationController
   end
 
   def show
-    @passenger_id = params[:id]
+    passenger_id = params[:id]
     @passenger = Passenger.find_by(id: passenger_id)
   end
 
