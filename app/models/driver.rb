6a98154e06a4c1911d607f3a277526b8ebad1a68
Maria Wissler <wmcarmelina@gmail.com>
2019-04-16 11:40:53 -0700

    added index for drivers, rating comment in model for driver, added comment in driver controller

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,23 @@
 class Driver < ApplicationRecord
   has_many :trips   # plural
   has_many :passengers, :through => :trips
+
+  # validates :name, presence: true
+  # validates :vin, presence: true
+
+  # def average_rating
+  #   #self is same thing as driver instance, since we are in model
+  #   sum = 0
+  #   self.trips.each do |trip|
+  #     sum = sum + trip.rating
+  #   end
+  #   length = self.trips.length
+
+  #   if length == 0
+  #     return 0
+  #   end
+
+  #   average = sum / length
+  #   return average
+  # end
 end
