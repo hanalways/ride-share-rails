5a137095bfa841baf2dcf0f5e00f2d234815d8d4
Hana <hanalways@gmail.com>
2019-04-16 10:41:09 -0700

    Update controller with new and create

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -7,4 +7,19 @@ class PassengersController < ApplicationController
     @passenger_id = params[:id]
     @passenger = Passenger.find_by(id: passenger_id)
   end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    @passenger.save
+    redirect_to passengers_path
+  end
+
+  private
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
 end
