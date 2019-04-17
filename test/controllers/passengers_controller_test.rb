61530b1644ad3932d726cdd7457f9d5e77b97fa5
Hana <hanalways@gmail.com>
2019-04-16 18:46:14 -0700

    index tests

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,12 +1,31 @@
 require "test_helper"
 
 describe PassengersController do
+  let (:new_passenger) {
+    Passenger.new(name: "Hana", phone_num: "206-555-5555")
+  }
+
   describe "index" do
-    # Your tests go here
+    it "renders without crashing" do
+      get passengers_path
+
+      must_respond_with :ok
+    end
+
+    it "renders even if there are no passengers" do
+      Passenger.destroy_all
+
+      get passengers_path
+      must_respond_with :ok
+    end
   end
 
   describe "show" do
-    # Your tests go here
+    it "renders for an existing passenger" do 
+      # get passenger_path(passenger.id)
+
+      # must_respond_with :ok
+    end
   end
 
   describe "edit" do
