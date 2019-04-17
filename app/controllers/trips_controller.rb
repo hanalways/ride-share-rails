057ac31fe03a7e9d2c84291ce145c935737ff625
Hana <hanalways@gmail.com>
2019-04-16 17:44:24 -0700

    add some new views

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,44 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    id = params[:id]
+    @trip = Trip.find(id)
+  end
+
+  def new
+    if params[:passenger_id]
+      passenger = Passenger.find_by(id: params[:passenger_id])
+      @trip = Trip.new
+    else
+      @trip = Trip.new
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.update(trip_params)
+      redirect_to passenger_path(@trip[:passenger_id])
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @trip = Trip.find_by(id: params[:id])
+    if @trip
+      @trip.destroy
+    end
+    redirect_to trips_path
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:id, :date, :rating, :cost, :driver_id, :passenger_id)
+  end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,5 @@ class Passenger < ApplicationRecord
   has_many :trips
   has_many :drivers, :through => :trips
   validates :name, presence: true, format: {with: /\A[a-zA-Z\.\'\-]{2,50}(?: [a-zA-Z\.\'\-]{2,50})+\Z/}
-  validates :phone_num, presence: true, numericality: true
+  validates :phone_num, presence: true
 end
