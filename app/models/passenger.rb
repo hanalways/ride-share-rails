d7af06827eceed9f310024587506a406196cf60b
Hana <hanalways@gmail.com>
2019-04-18 18:32:33 -0700

    logic for total amount and implemented in view

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,10 @@ class Passenger < ApplicationRecord
   has_many :drivers, :through => :trips
   validates :name, presence: true, format: {with: /\A[a-zA-Z\.\'\-]{2,50}(?: [a-zA-Z\.\'\-]{2,50})+\Z/}
   validates :phone_num, presence: true
+
+  def total_cost
+    return self.trips.sum do |trip|
+      trip.cost
+    end
+  end
 end
